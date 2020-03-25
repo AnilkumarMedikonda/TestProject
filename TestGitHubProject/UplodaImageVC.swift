1cfc31f826b12cea1dd689453e0500c433f59212
ANIL KUMAR MEDIKONDA <kumarmedikonda1812@gmail.com>
2020-03-25 21:59:34 +0530

    Upload images Completed

---
+++ TestGitHubProject/UplodaImageVC.swift
@@ -0,0 +1,34 @@
+//
+//  UplodaImageVC.swift
+//  TestGitHubProject
+//
+//  Created by ANIL KUMAR MEDIKONDA on 25/03/20.
+//  Copyright © 2020 ANIL KUMAR MEDIKONDA. All rights reserved.
+//
+
+import UIKit
+
+class UplodaImageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .red
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
